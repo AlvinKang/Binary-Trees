7fb50fab8d846abc0685be468791b1665cc11159
Alvin Kang <oneinallin1@gmail.com>
2018-07-06 08:05:47 -0700

    Implement iterative and recursive insert into BST

---
+++ Insert.js
@@ -0,0 +1,36 @@
+import Node from "./BinaryTreeNode";
+
+// ITERATIVE: Given a BST insert new node at the appropriate location with `value` as its value and return the tree (root)
+function insert(root, value) {
+  const node = new Node(value);
+
+  // If tree is empty, just return this node
+  if (root === null) return node;
+
+  let parent = null;
+  let current = root;
+
+  // Traverse down until parent points to leaf node of correct location
+  while (current !== null) {
+    parent = current;
+    if (value >= parent.value) current = current.right;
+    else current = current.left;
+  }
+
+  if (value >= parent.value) parent.right = node;
+  else parent.left = node;
+
+  return root;
+}
+
+// RECURSIVE
+function recursiveInsert(root, value) {
+  // If tree is empty, return just the new node
+  if (root === null) return new Node(value);
+  // Otherwise just recurse down the BST
+  else {
+    if (value >= root) root.right = recursiveInsert(root.right, value);
+    else root.left = recursiveInsert(root.left, value);
+    return root;
+  }
+}
