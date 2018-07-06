fbe02ede21597fc6a2a62fe92bc7caefdc69b24e
Alvin Kang <oneinallin1@gmail.com>
2018-07-06 08:32:07 -0700

    Implement size function that returns number of nodes in binary tree

---
+++ Size.js
@@ -0,0 +1,11 @@
+// This problem demonstrates simple binary tree traversal. Given a binary tree, count the number of nodes in the tree.
+
+function size(node) {
+  // BASE CASE: Reach end of tree
+  if (node === null) return 0;
+  else {
+    let leftSize = size(node.left);
+    let rightSize = size(node.right);
+    return 1 + leftSize + rightSize;
+  }
+}
